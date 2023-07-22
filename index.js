49c54e8900c87d18117de640519e6a6f032c16f7
Rylee94 <128653866+Rylee94@users.noreply.github.com>
2023-07-22 18:09:28 -0500

    updated schema, seeds, and added functions for grabbing table data

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Import the inquirer library for handling user input
 const inquirer = require("inquirer");
-const mySQL = require("mysql");
+const mysql = require("mysql");
+const cTable = require("console.table");
 
 // mySQL connection info
 const dbConnection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "abc123",
+  password: "Anoka12!",
   database: "employeeTracker",
 });
 
@@ -19,13 +20,11 @@ function startPrompt() {
       {
         type: "checkbox",
         name: "menuOptions",
-        message: [
+        message: "Select an option:",
+        choices: [
           "View all departments",
           "View all roles",
           "View all employees",
-          "Add a department",
-          "Add a role",
-          "Add an employee",
           "Add a department",
           "Add a role",
           "Add an employee",
@@ -34,32 +33,33 @@ function startPrompt() {
       },
     ])
     .then((answer) => {
-      // Use a switch statement to handle different menu options
-      switch (answer.menuOptions) {
-        case "View all departments":
-          viewAllDepartments();
-          break;
-        case "View all roles":
-          viewAllRoles();
-          break;
-        case "View all employees":
-          viewAllEmployees();
-          break;
-        case "Add a department":
-          addDepartment();
-          break;
-        case "Add a role":
-          addRole();
-          break;
-        case "Add an employee":
-          addEmployee();
-          break;
-        case "Update an employee role":
-          updateEmployee();
-          break;
-        default:
-          console.log("Invalid option selected.");
-      }
+      answer.menuOptions.forEach((selectedOption) => {
+        switch (selectedOption) {
+          case "View all departments":
+            viewAllDepartments();
+            break;
+          case "View all roles":
+            viewAllRoles();
+            break;
+          case "View all employees":
+            viewAllEmployees();
+            break;
+          case "Add a department":
+            addDepartment();
+            break;
+          case "Add a role":
+            addRole();
+            break;
+          case "Add an employee":
+            addEmployee();
+            break;
+          case "Update an employee role":
+            updateEmployee();
+            break;
+          default:
+            console.log("Invalid option selected.");
+        }
+      });
     });
 }
 
@@ -72,13 +72,42 @@ function startPrompt() {
 
 function viewAllDepartments() {
   dbConnection.query(
-    "SELECT * FROM department ORDER BY id",
+    "SELECT * FROM departments ORDER BY id",
     function (err, res) {
-      if (err) {
-        console.log('fetch departments ERROR')
-      }
-      console.table(res);
+      if (err) throw err;
+      console.log("department error");
+      const table = cTable.getTable(res);
+      console.log(table);
       startPrompt();
     }
   );
 }
+
+function viewAllRoles() {
+  dbConnection.query(
+    "SELECT role.id, role.title, role.salary, departments.name AS department FROM role JOIN departments ON role.department_id = departments.id",
+    function (err, res) {
+      if (err) throw err;
+      console.log("roles error");
+      const table = cTable.getTable(res);
+      console.log(table);
+      startPrompt();
+    }
+  );
+}
+
+function viewAllEmployees() {
+  dbConnection.query(
+    "SELECT employee.id AS employee_id, employee.first_name, employee.last_name, role.title, role.salary, departments.name AS department, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN departments ON role.department_id = departments.id LEFT JOIN employee manager ON employee.manager_id = manager.id",
+
+    function (err, res) {
+      if (err) throw err;
+      console.log("error");
+      const table = cTable.getTable(res);
+      console.log(table);
+      startPrompt();
+    }
+  );
+}
+
+startPrompt();
