937b3d5cc6e4c38add50c6d124f880e6ddb462e0
Rylee94 <128653866+Rylee94@users.noreply.github.com>
2023-07-28 20:45:08 -0500

    added add role and add department functions

--- index.js
+++ index.js
@@ -63,13 +63,7 @@ function startPrompt() {
     });
 }
 
-// add
-// functions
-// for
-// each
-// prompt
-// here
-
+// VIEW ALL DEPARTMENTS
 function viewAllDepartments() {
   dbConnection.query(
     "SELECT * FROM departments ORDER BY id",
@@ -83,6 +77,7 @@ function viewAllDepartments() {
   );
 }
 
+// VIEW ALL ROLES
 function viewAllRoles() {
   dbConnection.query(
     "SELECT roles.id, roles.title, roles.salary, departments.dep_name AS department FROM roles JOIN departments ON roles.department_id = departments.id",
@@ -96,6 +91,7 @@ function viewAllRoles() {
   );
 }
 
+// VIEW ALL EMPLOYEES
 function viewAllEmployees() {
   dbConnection.query(
     "SELECT employee.id AS employee_id, employee.first_name, employee.last_name, roles.title, roles.salary, departments.dep_name AS department, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN roles ON employee.roles_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id LEFT JOIN employee manager ON employee.manager_id = manager.id",
@@ -109,5 +105,84 @@ function viewAllEmployees() {
   );
 }
 
+// ADD NEW DEPARTMENT
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "What department would you like to add?",
+      },
+    ])
+    .then(function (res) {
+      let query = dbConnection.query(
+        "INSERT INTO departments SET ?",
+        {
+          dep_name: res.name,
+        },
+        function (err, result) {
+          if (err) throw err;
+          console.log("Department added successfully!");
+          startPrompt();
+        }
+      );
+    });
+}
+
+// ADD NEW ROLE
+function addRole() {
+  // Retrieve department names and IDs from the database
+  dbConnection.query(
+    "SELECT id, dep_name FROM departments",
+    function (err, departments) {
+      if (err) throw err;
+
+      // Map department names to their corresponding IDs
+      const departmentChoices = departments.map((department) => ({
+        name: department.dep_name,
+        value: department.id,
+      }));
+
+      inquirer
+        .prompt([
+          {
+            name: "title",
+            type: "input",
+            message: "What is the title of the new role?",
+          },
+          {
+            name: "salary",
+            type: "input",
+            message: "What is the salary of the new role?",
+          },
+          {
+            name: "department_id",
+            type: "list", // Use a list to display department choices
+            message: "What department will the new role be a part of?",
+            choices: departmentChoices,
+          },
+        ])
+        .then(function (res) {
+          // Convert salary to a numeric value without commas
+          const salary = parseFloat(res.salary.replace(",", ""));
+
+          dbConnection.query(
+            "INSERT INTO roles SET ?",
+            {
+              title: res.title,
+              salary: salary,
+              department_id: res.department_id,
+            },
+            function (err, result) {
+              if (err) throw err;
+              console.log("Role added successfully!");
+              startPrompt();
+            }
+          );
+        });
+    }
+  );
+}
+
 startPrompt();
- 
